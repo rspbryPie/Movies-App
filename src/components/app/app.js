cba334ac5d9c10f640a03cbd5a87137fd1a13e90
Sergey Savko <serg.savko@gmail.com>
2022-10-07 17:24:40 +0300

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import ItemList from '../item-list';
+
+import './app.css';
+
+const App = () => {
+  return (
+    <div>
+      hello world
+      <ItemList />
+    </div>
+  );
+};
+
+export default App;
